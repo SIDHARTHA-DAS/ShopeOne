c86e21594fad63d5f4516848d7a29d93c4780dd6
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2024-09-08 09:07:30 +0530

    add

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 import './style.css'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-   
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
